8dc61490a63e7b34b1192d16efbc43b5b143f2fa
noname-v2 <libccy@outlook.com>
2025-09-19 03:16:29 -0400

    Update documentation.

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,7 @@
+/**
+ * This file will be copied to <platform>/_client.js for each platform
+ * to initialize their custom Client object and is not intended to be used directly.
+ */
 import Client from './client';
 
 const client = new Client();
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,7 @@
+/**
+ * This file will be copied to <platform>/_server.js for each platform
+ * to initialize their custom Server object and is not intended to be used directly.
+ */
 import Server from './server';
 
 const server = new Server();
